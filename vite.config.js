1fa1053c44a9cb3342f15e195b07f9d5995b9f4a
RendraNurYuliadi <rendra.education@gmail.com>
2026-05-21 15:42:40 +0700

    Build site to docs for GitHub Pages source

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '/DigitalSolution/',
   plugins: [react()],
+  build: {
+    outDir: 'docs'
+  },
   server: {
     port: 5173
   }
